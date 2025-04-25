d92a79740e6efdca044b1181ab9d5a5a1b369a85
DamianPlis <damian.plis.praha@gmail.com>
2025-04-25 21:06:44 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,86 @@
+function playGame (myMove) {
+    const PCmove = pickPCmove()
+
+    let result = ""
+
+    if (myMove === "rock") {
+        if (PCmove === "paper") {
+            result = "You lose"
+        } else if (PCmove === "scissors") {
+            result = "You Win"
+        } else if (PCmove === "rock") {
+            result = "Tie"
+        }
+    } else if (myMove === "paper") {
+        if (PCmove === "paper") {
+            result = "Tie"
+        } else if (PCmove === "scissors") {
+            result = "You lose "
+        } else if (PCmove === "rock") {
+            result = "You Win"
+        }
+    } else if (myMove === "scissors") {
+        if (PCmove === "paper") {
+            result = "You Win"
+        } else if (PCmove === "scissors") {
+            result = "Tie"
+        } else if (PCmove === "rock") {
+            result = "You lose"
+        }
+    }
+
+    if (result === "You Win") {
+        score.wins += 1;
+    } else if (result === "Tie") {
+        score.ties += 1;
+    } else if (result === "You lose") {
+        score.losses += 1;
+    }
+
+    localStorage.setItem("score", JSON.stringify(score))
+
+    alert(`You pickes ${myMove}. The computer picked ${PCmove}. ${result}
+Wins: ${score.wins} Losses: ${score.losses} Ties ${score.ties}`)
+
+resultElem.innerHTML = `${result}`
+picksElem.innerHTML = `You picked ${myMove}. The coumputer picked ${PCmove}`
+scoreElem.innerHTML = `Wins: ${score.wins} Losses: ${score.losses} Ties ${score.ties}`
+} 
+
+
+function pickPCmove () {
+    const randomNumber = Math.random()
+
+    let PCmove = ""
+
+    if (randomNumber < 1 / 3 ) {
+        PCmove = "rock"
+    } else if (randomNumber > 1 / 3 && randomNumber < 2 / 3 ) {
+        PCmove = "paper"
+    } else if (randomNumber > 2 / 3 ) {
+        PCmove = "scissors"
+    }
+
+    return PCmove;
+}
+
+function resetScore() {
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0,
+    }
+    scoreElem.innerHTML = `Wins: ${score.wins} Losses: ${score.losses} Ties ${score.ties}`
+}
+
+let score = JSON.parse(localStorage.getItem("score")) || {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+};
+
+const scoreElem = document.querySelector(".W-L-T")
+const picksElem = document.querySelector(".picks")
+const resultElem = document.querySelector(".result")
+
+scoreElem.innerHTML = `Wins: ${score.wins} Losses: ${score.losses} Ties ${score.ties}`
---
+++ loops.js
@@ -0,0 +1,46 @@
+let todoList = []
+displayTodoList();
+function addTodo() {
+    console.log(todoList)
+
+    const input1 = document.querySelector("#input")
+    const input = document.querySelector("#input").value
+
+    const date1 = document.querySelector("#date")
+    const date = document.querySelector("#date").value
+
+    if (input === "" || date === "") {
+        return;
+    }
+    todoList.push({
+        input,
+        date,
+    })
+    input1.value = "";
+    date1.value = "";
+    
+    displayTodoList();
+}
+
+function displayTodoList() {
+    let todoListRenderedHTML = ""
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoNumbers = todoList[i].input
+        const todoDate = todoList[i].date
+        todoListRenderedHTML += `<div><p style= display:inline-block;>${todoNumbers}</p> <p style="display:inline-block;">${todoDate}</p><button class="delete-button" onclick="deleteTodo(${i})">Delete</button></div>`
+    }
+
+    document.querySelector("#insert-div").innerHTML = todoListRenderedHTML;
+}
+
+function onKeyDown(event) {
+    if (event.key === "Enter") {
+        addTodo();
+    }
+}
+
+function deleteTodo(index) {
+    todoList.splice(index, 1);
+    displayTodoList();
+}
