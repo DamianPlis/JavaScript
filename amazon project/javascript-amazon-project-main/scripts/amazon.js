cef78eb164d4ee96f4834512946c3c80ea44c6c0
DamianPlis <damian.plis.praha@gmail.com>
2025-05-03 12:22:18 +0200

    fix

--- amazon project/javascript-amazon-project-main/scripts/amazon.js
+++ amazon project/javascript-amazon-project-main/scripts/amazon.js
@@ -68,7 +68,7 @@ products.forEach((products, i) => {
         </div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${products.rating * 10}.png">
+          <img class="product-rating-stars" src="../images/ratings/rating-${products.stars * 10}.png">
           <div class="product-rating-count link-primary">
             ${products.rating.count}
           </div>
